d7fe05b75c14d46856d76588a50fee0645c02ccc
Michael J Long <michael.long@greymatter.io>
2022-03-21 11:42:26 -0400

    Adds working exercise 3.4

--- pkg/algorithms/chapter1/resident_hospital_matching.go
+++ pkg/algorithms/chapter1/resident_hospital_matching.go
@@ -15,12 +15,13 @@ type Hospital struct { //man
 }
 
 type Resident struct { //woman
-	Id          string
-	Preferences []*Hospital //An array of hospital preferences for a resident. All the hospitals are in this array.
-	Hospital    *Hospital   //The hospital the resident is currently assigned to
+	Id string
+	//HospitalPreferences map[string]int//A raking of hospital preferences for a resident. All the hospitals are in this map. Key is hospital ID and ranking is like a golf score, low is better.
+	HospitalPreferences []*Hospital //An array of hospital preferences for a resident. All the hospitals are in this array.
+	Hospital            *Hospital   //The hospital the resident is currently assigned to
 }
 
-func MatchResidentToHospitals(hospitalsWithResidentOpenings *linked_list.LinkedList[*Hospital]) []*Resident {
+func MatchResidentsToHospitals(hospitalsWithResidentOpenings *linked_list.LinkedList[*Hospital]) []*Resident {
 	fmt.Printf("Size of list:%v\n", linked_list.Len(hospitalsWithResidentOpenings))
 
 	//Invariants
@@ -53,63 +54,53 @@ func MatchResidentToHospitals(hospitalsWithResidentOpenings *linked_list.LinkedL
 	}
 
 	residentPrefersThisHospital := func(res *Resident, thisHospital *Hospital) bool { //Does resident prefer this hospital to the one to which he is currently assigned?
-		var result bool
-		for _, m := range res.Preferences {
-			if mEq(res.Hospital, thisHospital) || mEq(res.Hospital, m) {
-				if mEq(m, thisHospital) {
-					result = true
-					fmt.Printf("resident:%v prefers this hospital:%v over current one:%v\n", res.Id, thisHospital.Id, m.Id)
-					break
-				} else if mEq(m, res.Hospital) {
-					fmt.Printf("resident:%v prefers current hospital:%v over this one:%v\n", res.Id, m.Id, res.Hospital.Id)
-					result = false
-					break
-				}
+		var thisHospitalRanking = -1
+		var currentHospitalRanking = -1
+		for i, m := range res.HospitalPreferences {
+			if mEq(m, thisHospital) {
+				thisHospitalRanking = i
+			} else if mEq(res.Hospital, m) {
+				currentHospitalRanking = i
+			}
+			if thisHospitalRanking != -1 && currentHospitalRanking != -1 {
+				break
 			}
 		}
-		return result
+		if thisHospitalRanking < currentHospitalRanking {
+			fmt.Printf("resident:%v prefers this hospital:%v over current one:%v\n", res.Id, thisHospital.Id, res.Hospital.Id)
+			return true
+		} else {
+			fmt.Printf("resident:%v prefers current hospital:%v over this one:%v\n", res.Id, res.Hospital.Id, thisHospital.Id)
+			return false
+		}
 	}
 
 	for hospitalsWithResidentOpenings != nil {
-		hospital := linked_list.Head(hospitalsWithResidentOpenings)
-		var hospitalResidentPreferences = hospital.ResidentPreferences
-		var emptyStackError error
-		for hospitalResidentPreferences != nil && len(hospital.Residents) <= hospital.ResidentCapacity { //Loop over the hospital's resident preferences until the hospital has reached it' capacity
+		thisHospital := linked_list.Head(hospitalsWithResidentOpenings)
+		var hospitalResidentPreferences = thisHospital.ResidentPreferences
+		for hospitalResidentPreferences != nil && len(thisHospital.Residents) < thisHospital.ResidentCapacity { //Loop over the hospital's resident preferences until the hospital has reached it' capacity
 			resident := linked_list.Head(hospitalResidentPreferences)
-			if resident.Hospital == nil && len(hospital.Residents) <= hospital.ResidentCapacity {
-				resident.Hospital = hospital
-				hospitalResidentPreferences, emptyStackError = linked_list.Tail(hospitalResidentPreferences)
-				if emptyStackError != nil {
-					fmt.Printf("1:%v\n", emptyStackError)
-				}
-				hospital.Residents[resident.Id] = resident
+			if resident.Hospital == nil {
+				resident.Hospital = thisHospital
+				hospitalResidentPreferences, _ = linked_list.Tail(hospitalResidentPreferences)
+				thisHospital.Residents[resident.Id] = resident
 			} else {
 				//Does this resident prefer this hospital to the one to whom he is currently assigned? If so he
 				//breaks his agreement to that hospital and you make hospital have an additional resident opening.
 				//Otherwise just try the next hospital in the current resident's non-proposed-to(preferences) stack.
-				if residentPrefersThisHospital(resident, hospital) {
+				if residentPrefersThisHospital(resident, thisHospital) {
 					oldHospital := resident.Hospital
 					delete(oldHospital.Residents, resident.Id)
 					///Set up current resident with this hospital
-					resident.Hospital = hospital
-					hospitalsWithResidentOpenings = linked_list.AddLast(oldHospital, hospitalsWithResidentOpenings) //TODO Might have to check to see if this hospital is already in the hospitalsWithResidentOpenings list
-					fmt.Printf("Resident %v prefers hospital(%v) and just broke agreement with hospital %v\n", resident.Id, hospital.Id, oldHospital.Id)
-				} else {
-					fmt.Printf("Resident %v prefers her current hospital %v over this one (%v)\n", resident.Id, resident.Hospital.Id, hospital.Id)
+					resident.Hospital = thisHospital
+					thisHospital.Residents[resident.Id] = resident
+					hospitalsWithResidentOpenings = linked_list.AddLast(oldHospital, hospitalsWithResidentOpenings)
 				}
 			}
-			//hospital.ResidentPreferences, emptyStackError = linked_list.Tail(hospital.ResidentPreferences)
-			hospitalResidentPreferences, emptyStackError = linked_list.Tail(hospital.ResidentPreferences)
-			hospital.ResidentPreferences = hospitalResidentPreferences
-			if emptyStackError != nil {
-				break //fmt.Printf("2:%v\n", emptyStackError)
-			}
-
+			hospitalResidentPreferences, _ = linked_list.Tail(thisHospital.ResidentPreferences)
+			thisHospital.ResidentPreferences = hospitalResidentPreferences
 		} //end resident for
-		hospitalsWithResidentOpenings, emptyStackError = linked_list.Tail(hospitalsWithResidentOpenings)
-		if emptyStackError != nil {
-			fmt.Printf("3:%v\n", emptyStackError)
-		}
+		hospitalsWithResidentOpenings, _ = linked_list.Tail(hospitalsWithResidentOpenings)
 	} // end hospital with resident openings for
 	return allResidents
 }
--- pkg/algorithms/chapter1/resident_hospital_matching_test.go
+++ pkg/algorithms/chapter1/resident_hospital_matching_test.go
@@ -1,36 +1,36 @@
 package chapter1
 
 import (
-	"fmt"
+	"github.com/greymatter-io/golangz/arrays"
 	"github.com/greymatter-io/golangz/linked_list"
 	"testing"
 )
 
 func TestResidentHospitalMatching(t *testing.T) {
 	res1 := &Resident{
-		Id:          "1",
-		Preferences: nil,
-		Hospital:    nil,
+		Id:                  "1",
+		HospitalPreferences: nil,
+		Hospital:            nil,
 	}
 	res2 := &Resident{
-		Id:          "2",
-		Preferences: nil,
-		Hospital:    nil,
+		Id:                  "2",
+		HospitalPreferences: nil,
+		Hospital:            nil,
 	}
 	res3 := &Resident{
-		Id:          "3",
-		Preferences: nil,
-		Hospital:    nil,
+		Id:                  "3",
+		HospitalPreferences: nil,
+		Hospital:            nil,
 	}
 	res4 := &Resident{
-		Id:          "4",
-		Preferences: nil,
-		Hospital:    nil,
+		Id:                  "4",
+		HospitalPreferences: nil,
+		Hospital:            nil,
 	}
 	res5 := &Resident{
-		Id:          "5",
-		Preferences: nil,
-		Hospital:    nil,
+		Id:                  "5",
+		HospitalPreferences: nil,
+		Hospital:            nil,
 	}
 
 	hosp1 := &Hospital{
@@ -46,30 +46,46 @@ func TestResidentHospitalMatching(t *testing.T) {
 		ResidentPreferences: nil,
 	}
 
-	res1.Preferences = []*Hospital{hosp2, hosp1}
-	res2.Preferences = []*Hospital{hosp2, hosp1}
-	res3.Preferences = []*Hospital{hosp1, hosp2}
-	res4.Preferences = []*Hospital{hosp1, hosp2}
-	res5.Preferences = []*Hospital{hosp1, hosp2}
+	res1.HospitalPreferences = []*Hospital{hosp2, hosp1}
+	res2.HospitalPreferences = []*Hospital{hosp2, hosp1}
+	res3.HospitalPreferences = []*Hospital{hosp1, hosp2}
+	res4.HospitalPreferences = []*Hospital{hosp1, hosp2}
+	res5.HospitalPreferences = []*Hospital{hosp1, hosp2}
 
 	var hospitalsWithResidentOpenings *linked_list.LinkedList[*Hospital]
 	var hosp1ResPrefs *linked_list.LinkedList[*Resident]
-	hosp1ResPrefs = linked_list.Push(res1, hosp1ResPrefs)
-	hosp1ResPrefs = linked_list.Push(res2, hosp1ResPrefs)
-	hosp1ResPrefs = linked_list.Push(res3, hosp1ResPrefs)
-	hosp1ResPrefs = linked_list.Push(res4, hosp1ResPrefs)
 	hosp1ResPrefs = linked_list.Push(res5, hosp1ResPrefs)
+	hosp1ResPrefs = linked_list.Push(res4, hosp1ResPrefs)
+	hosp1ResPrefs = linked_list.Push(res3, hosp1ResPrefs)
+	hosp1ResPrefs = linked_list.Push(res2, hosp1ResPrefs)
+	hosp1ResPrefs = linked_list.Push(res1, hosp1ResPrefs)
 
 	hosp1.ResidentPreferences = hosp1ResPrefs
 	hosp2.ResidentPreferences = hosp1ResPrefs //TODO Fix this to be different
-	hospitalsWithResidentOpenings = linked_list.Push(hosp1, hospitalsWithResidentOpenings)
 	hospitalsWithResidentOpenings = linked_list.Push(hosp2, hospitalsWithResidentOpenings)
-	match := MatchResidentToHospitals(hospitalsWithResidentOpenings)
-	for _, y := range match {
-		fmt.Println(y.Hospital.Id)
+	hospitalsWithResidentOpenings = linked_list.Push(hosp1, hospitalsWithResidentOpenings)
+	MatchResidentsToHospitals(hospitalsWithResidentOpenings)
+	var hosp1Residents []string
+	var hosp2Residents []string
 
+	eq := func(l, r string) bool {
+		if l == r {
+			return true
+		} else {
+			return false
+
+		}
 	}
-	fmt.Println()
-	fmt.Println(match)
-	t.Errorf("fail")
+	for _, y := range hosp1.Residents {
+		hosp1Residents = append(hosp1Residents, y.Id)
+	}
+	for _, y := range hosp2.Residents {
+		hosp2Residents = append(hosp2Residents, y.Id)
+	}
+	if !arrays.SetEquality(hosp1Residents, []string{"4", "3"}, eq) {
+		t.Errorf("Actual:%v Expected:%v", hosp1Residents, []string{"4", "3"})
+	}
+	if !arrays.SetEquality(hosp2Residents, []string{"2", "1"}, eq) {
+		t.Errorf("Actual:%v Expected:%v", hosp2Residents, []string{"2", "1"})
+	}
 }
