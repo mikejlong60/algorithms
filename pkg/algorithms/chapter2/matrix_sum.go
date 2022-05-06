7774c2b6df031712c6f4e5e5919d82e588b6d0ce
Michael J Long <michael.long@greymatter.io>
2022-05-06 09:02:53 -0400

    More efficient algorithm for summing

--- pkg/algorithms/chapter2/matrix_sum.go
+++ pkg/algorithms/chapter2/matrix_sum.go
@@ -1,6 +1,6 @@
 package chapter2
 
-//Big O for this algorithm is O(2(N-1))
+//Big O for this algorithm is O(2 times n to (N-1)th power)
 func sum(source []int) [][]int64 {
 	var result = make([][]int64, len(source))
 	for i, j := range source {
@@ -11,6 +11,21 @@ func sum(source []int) [][]int64 {
 			sum = sum + int64(source[k])
 		}
 		result[i][1] = sum
+	}
+	return result
+}
+
+//This algorithm has a constant running time of O(n)
+func betterMatrixSum(source []int) [][]int64 {
+	var result = make([][]int64, len(source))
+	for i, j := range source {
+		result[i] = make([]int64, 2)
+		result[i][0] = int64(j)
+		if i == 0 { //Set first sum to first element value
+			result[i][1] = result[i][0]
+		} else { //Just grqab previous sum
+			result[i][1] = result[i-1][1] + result[i][0]
+		}
 	}
 	return result
 }
--- pkg/algorithms/chapter2/matrix_sum_test.go
+++ pkg/algorithms/chapter2/matrix_sum_test.go
@@ -8,21 +8,61 @@ import (
 	"time"
 )
 
-func TestSumMatrix(t *testing.T) {
+func TestMatrixSum(t *testing.T) {
 
 	g0 := propcheck.ChooseInt(1, 3000)
-	g1 := propcheck.ChooseArray(0, 1000, g0)
+	g1 := propcheck.ChooseArray(0, 10000, g0)
 	now := time.Now().Nanosecond()
 	rng := propcheck.SimpleRNG{now}
 	prop := propcheck.ForAll(g1,
 		"Validate exercise 2.6, a sort-of sum of a matrix  \n",
 		func(xs []int) [][]int64 {
-			return sum(xs)
+			start := time.Now()
+			r := sum(xs)
+			fmt.Printf("Summing algorithm for an array of length:%v took:%v\n", len(xs), time.Since(start))
+			return r
 		},
 		func(xss [][]int64) (bool, error) {
+			//fmt.Println(xss)
 			var errors error
 			for i := 1; i < len(xss); i++ {
 				last := xss[i-1][1]
+
+				if xss[i][1] < last {
+					errors = multierror.Append(errors, fmt.Errorf("Array element sum[%v] should not have been less than previous accumulated value", xss[i][1]))
+				}
+			}
+			if errors != nil {
+				return false, errors
+			} else {
+				return true, nil
+			}
+		},
+	)
+	result := prop.Run(propcheck.RunParms{100, rng})
+	propcheck.ExpectSuccess[[]int](t, result)
+}
+
+func TestMoreEfficientMatrixSum(t *testing.T) {
+
+	g0 := propcheck.ChooseInt(1, 3000)
+	g1 := propcheck.ChooseArray(0, 10000, g0)
+	now := time.Now().Nanosecond()
+	rng := propcheck.SimpleRNG{now}
+	prop := propcheck.ForAll(g1,
+		"Validate exercise 2.6, a sort-of sum of a matrix. This is more efficient that the previous version  \n",
+		func(xs []int) [][]int64 {
+			start := time.Now()
+			r := betterMatrixSum(xs)
+			fmt.Printf("Summing algorithm for an array of length:%v took:%v\n", len(xs), time.Since(start))
+			return r
+		},
+		func(xss [][]int64) (bool, error) {
+			//fmt.Println(xss)
+			var errors error
+			for i := 1; i < len(xss); i++ {
+				last := xss[i-1][1]
+
 				if xss[i][1] < last {
 					errors = multierror.Append(errors, fmt.Errorf("Array element sum[%v] should not have been less than previous accumulated value", xss[i][1]))
 				}
