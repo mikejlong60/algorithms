a52c5265eff7a0190828d5230a8ce9dccbb67e6b
Michael J Long <michael.long@greymatter.io>
2022-05-24 15:46:03 -0400

    Adds HeapInsert

--- pkg/algorithms/chapter2/heap.go
+++ pkg/algorithms/chapter2/heap.go
@@ -14,33 +14,33 @@ func ParentIdx(i int) int {
 //node is at the bottom rung of the heap.
 //Definition of a heap.  Every node in the tree has a greater value than it's parent as per the lt function.
 //This is a pure function because I copy the array each time.  r only gets mutated internally which is OK because its a copy.
-func HeapifyUp(heap []int, i int) []int {
+func HeapifyUp[A any](heap []A, i int, lt func(l, r A) bool) []A {
 	if len(heap) == 0 {
-		return []int{}
+		return []A{}
 	}
-	r := make([]int, len(heap))
+	r := make([]A, len(heap))
 	copy(r, heap)
 
 	if i > 1 {
 		j := ParentIdx(i)
-		if r[i] < r[j] {
+		if lt(r[i], r[j]) {
 			//Swap elements
 			temp := r[i]
 			temp2 := r[j]
 			r[j] = temp
 			r[i] = temp2
-			r = HeapifyUp(r, j)
+			r = HeapifyUp(r, j, lt)
 		}
 	}
 	return r
 }
 
 //This is a pure function because I copy the array each time.  r only gets mutated internally which is OK because its a copy.
-func HeapifyDown(heap []int, i int) []int {
+func HeapifyDown[A any](heap []A, i int, lt func(l, r A) bool) []A {
 	if len(heap) == 0 {
-		return []int{}
+		return []A{}
 	}
-	r := make([]int, len(heap))
+	r := make([]A, len(heap))
 	copy(r, heap)
 
 	var j int
@@ -52,7 +52,7 @@ func HeapifyDown(heap []int, i int) []int {
 		rightIdx := 2*i + 1
 		leftVal := r[leftIdx]
 		rightVal := r[rightIdx]
-		if leftVal < rightVal {
+		if lt(leftVal, rightVal) {
 			j = leftIdx
 		} else {
 			j = rightIdx
@@ -62,13 +62,44 @@ func HeapifyDown(heap []int, i int) []int {
 	}
 	val1 := r[j]
 	val2 := r[i]
-	if val1 < val2 {
+	if lt(val1, val2) {
 		//Swap elements
 		temp := r[i]
 		temp2 := r[j]
 		r[j] = temp
 		r[i] = temp2
-		r = HeapifyDown(r, j)
+		r = HeapifyDown(r, j, lt)
 	}
 	return r
+}
+
+//Make an empty heap to store at most n elements
+func StartHeap[A any](n int) []A {
+	return make([]A, n)
+}
+
+//Returns the minimum element in the given heap without removing it
+func HeapMin[A any](heap []A) A {
+	return heap[0]
+}
+
+//Inserts an element into the given heap and returns a new heap. This is a pure funciton
+func HeapInsert[A any](heap []A, a A, lt func(l, r A) bool, isZeroVal func(a A) bool) []A {
+	if len(heap) == 0 {
+		return []A{}
+	}
+	r := make([]A, len(heap))
+	copy(r, heap)
+
+	findFirstEmptySlotInHeap := func(h []A) int {
+		for i, x := range h {
+			if isZeroVal(x) {
+				return i
+			}
+		}
+		panic("heap is already full. You must copy the old heap to a new larger heap and use that to HeapInsert your new element.")
+	}
+	l := findFirstEmptySlotInHeap(r)
+	r[l] = a
+	return HeapifyUp(r, l, lt)
 }
--- pkg/algorithms/chapter2/heap_test.go
+++ pkg/algorithms/chapter2/heap_test.go
@@ -3,17 +3,26 @@ package chapter2
 import (
 	"fmt"
 	"github.com/greymatter-io/golangz/propcheck"
+	"github.com/greymatter-io/golangz/sorting"
 	"github.com/hashicorp/go-multierror"
 	"testing"
 	"time"
 )
+
+var lt = func(l, r int) bool {
+	if l < r {
+		return true
+	} else {
+		return false
+	}
+}
 
 var parentIsLess = func(heap []int, lastIdx int) error {
 	var parentIdx = ParentIdx(lastIdx)
 	var childIdx = lastIdx
 	var errors error
 	for parentIdx > 0 {
-		fmt.Printf("parents value:%v child's value:%v\n", heap[parentIdx], heap[childIdx])
+		//		fmt.Printf("parents value:%v child's value:%v\n", heap[parentIdx], heap[childIdx])
 		if heap[parentIdx] > heap[childIdx] {
 			errors = multierror.Append(errors, fmt.Errorf("parent:%v value was not less than child's:%v\n", heap[parentIdx], heap[childIdx]))
 		}
@@ -36,7 +45,7 @@ func TestHeapifyUpOddNumberBottomNodes(t *testing.T) {
 	var elem = 15 //The element you want to add to the almost-a-heap. It's always 3 but position shifts
 	var errors error
 	for _, xss := range [][]int{xss0, xss1, xss2, xss3, xss4, xss5, xss6, xss7} {
-		r := HeapifyUp(xss, elem)
+		r := HeapifyUp(xss, elem, lt)
 		fmt.Println(r)
 		errors = parentIsLess(r, elem)
 		elem = elem - 1
@@ -59,7 +68,7 @@ func TestHeapifyUpEvenNumberBottomNodes(t *testing.T) {
 	var elem = 14 //The element you want to add to the almost-a-heap. It's always 3 but position shifts
 	var errors error
 	for _, xss := range [][]int{xss0, xss1, xss2, xss3, xss4, xss5, xss6, xss7} {
-		r := HeapifyUp(xss, elem)
+		r := HeapifyUp(xss, elem, lt)
 		fmt.Println(r)
 		errors = parentIsLess(r, elem)
 		elem = elem - 1
@@ -79,7 +88,7 @@ func TestHeapifyDown(t *testing.T) {
 			lastIdx := (len(xss) - 1) / 2
 			fmt.Println(rng)
 			fmt.Printf("Array Before HeapifyDown   :%v\n", xss)
-			r := HeapifyDown(xss, lastIdx)
+			r := HeapifyDown(xss, lastIdx, lt)
 			fmt.Printf("HeapifyDown algorithm for an array of length:%v took:%v\n", len(xss), time.Since(start))
 			fmt.Printf("Array After HeapifyDown    :%v\n", xss)
 			fmt.Printf("Array After PureHeapifyDown:%v\n", r)
@@ -97,6 +106,66 @@ func TestHeapifyDown(t *testing.T) {
 		},
 	)
 	result := prop.Run(propcheck.RunParms{1, rng})
+	propcheck.ExpectSuccess[[]int](t, result)
+	fmt.Println(rng)
+}
+
+func TestHeapInsertAndFindMin(t *testing.T) {
+	isZeroVal := func(s int) bool {
+		if s == 0 {
+			return true
+		} else {
+			return false
+		}
+	}
+	var lt = func(l, r int) bool {
+		if l < r {
+			return true
+		} else {
+			return false
+		}
+	}
+
+	insertIntoHeap := func(xss []int) []int {
+		var r = StartHeap[int](len(xss))
+		start := time.Now()
+		for _, x := range xss {
+			r = HeapInsert(r, x, lt, isZeroVal)
+		}
+		fmt.Printf("Heap HeapInsert algorithm for an array of length:%v took:%v\n", len(xss), time.Since(start))
+		return r
+	}
+
+	parentLess := func(xss []int) (bool, error) {
+		lastIdx := len(xss) - 1
+		errors := parentIsLess(xss, lastIdx)
+		if errors != nil {
+			return false, errors
+		} else {
+			return true, nil
+		}
+	}
+	topIsMin := func(heap []int) (bool, error) {
+		s := sorting.QuickSort(heap, lt)
+		var errors error
+		if s[0] != HeapMin(heap) {
+			errors = multierror.Append(errors, fmt.Errorf("top of heap was not smallest. expected:%v actual:%v\n", s[0], heap[0]))
+		}
+		if errors != nil {
+			return false, errors
+		} else {
+			return true, nil
+		}
+	}
+
+	g0 := propcheck.ChooseArray(5, 1000, propcheck.ChooseInt(1, 10000))
+	rng := propcheck.SimpleRNG{time.Now().Nanosecond()}
+	prop := propcheck.ForAll(g0,
+		"Validate HeapMin  \n",
+		insertIntoHeap,
+		propcheck.AssertionAnd(parentLess, topIsMin),
+	)
+	result := prop.Run(propcheck.RunParms{100, rng})
 	propcheck.ExpectSuccess[[]int](t, result)
 	fmt.Println(rng)
 }
