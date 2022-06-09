3cfa6e9cb5409c7eab710a753068d1319b3c991f
Michael J Long <michael.long@greymatter.io>
2022-06-09 10:16:29 -0400

    Breaking point with budget of 2 broken jars

--- pkg/algorithms/chapter2/max_drop_test.go
+++ pkg/algorithms/chapter2/max_drop_test.go
@@ -6,10 +6,11 @@ import (
 	"github.com/greymatter-io/golangz/sets"
 	"github.com/hashicorp/go-multierror"
 	"testing"
+	"time"
 )
 
 func findBreakingPointWithoutBreakingJar(ladder []int, breakingPoint int) int {
-	breakingPointIsHigher := ladder[0] < breakingPoint
+	breakingPointIsHigher := ladder[0] <= breakingPoint
 	var wrungB4BreakingPoint = -1
 
 	if breakingPointIsHigher {
@@ -36,7 +37,7 @@ func highestBreakingPoint(ladder []int, breakingPoint, budget, usedBudget int) i
 	} else { //Divide in half again
 		lowerHalf := ladder[0 : len(ladder)/2]
 		upperHalf := ladder[len(ladder)/2:]
-		if breakingPoint < upperHalf[0] {
+		if breakingPoint <= upperHalf[0] {
 			return highestBreakingPoint(lowerHalf, breakingPoint, budget, usedBudget+1)
 		} else {
 			return highestBreakingPoint(upperHalf, breakingPoint, budget, usedBudget+1)
@@ -69,7 +70,7 @@ func TestMaxDropGivenBudget(t *testing.T) { //This reverses the order because fo
 		i := func(x int) propcheck.Pair[[]int, int] {
 			return propcheck.Pair[[]int, int]{
 				xss,
-				x,
+				xss[x],
 			}
 		}
 		h := propcheck.Map(g, i)
@@ -77,24 +78,23 @@ func TestMaxDropGivenBudget(t *testing.T) { //This reverses the order because fo
 	}
 
 	g2 := propcheck.FlatMap(g1, f)
-	//now := time.Now().Nanosecond()
-	rng := propcheck.SimpleRNG{197695000} //now}
+	now := time.Now().Nanosecond()
+	rng := propcheck.SimpleRNG{now}
 	fmt.Println(rng)
 	budget := 2 //TODO make this a random value between 2 and the size of the generated array.
 	prop := propcheck.ForAll(g2,
 		"Exercise 2.8b, the max jar drop given a budget of not-to-exceed broken jars.",
 		func(xs propcheck.Pair[[]int, int]) propcheck.Pair[int, propcheck.Pair[[]int, int]] {
 			//A is the ladder, B is the breaking point on the ladder(the actual value in the array, not its index).
-			r := highestBreakingPoint(xs.A, xs.A[xs.B], budget, 0)
+			r := highestBreakingPoint(xs.A, xs.B, budget, 0)
 			return propcheck.Pair[int, propcheck.Pair[[]int, int]]{r, xs}
 		},
 		func(highestWrungAEtAll propcheck.Pair[int, propcheck.Pair[[]int, int]]) (bool, error) {
 			var errors error
-			indexOfHighestWrung := highestWrungAEtAll.B.B - 1
-			ladder := highestWrungAEtAll.B.A
+			breakingPoint := highestWrungAEtAll.B.B
 			highestWrung := highestWrungAEtAll.A
-			if highestWrung != ladder[indexOfHighestWrung] {
-				errors = multierror.Append(errors, fmt.Errorf("Expected highest non-breaking wrung withing budget to be:%v but was:%v", highestWrung, ladder[indexOfHighestWrung]))
+			if highestWrung >= breakingPoint {
+				errors = multierror.Append(errors, fmt.Errorf("Expected highest non-breaking wrung withing budget to be:%v but was:%v", highestWrung, breakingPoint))
 			}
 			if errors != nil {
 				return false, errors
