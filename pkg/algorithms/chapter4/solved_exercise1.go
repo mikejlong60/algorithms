61dacc39d2c61be831837f53835380806c3de3c4
mikejlong60 <mikejlong60@gmail.com>
2022-11-17 12:52:04 -0500

    Improves algorithm by not using global variable

--- pkg/algorithms/chapter4/solved_exercise1.go
+++ pkg/algorithms/chapter4/solved_exercise1.go
@@ -1,9 +1,11 @@
 package chapter4
 
-var numberOfSteps int
+import (
+	"fmt"
+	"sync/atomic"
+)
 
-func peakOfAList(xs []int) int {
-	numberOfSteps = numberOfSteps + 1
+func peakOfAList(xs []int, currentGoRoutines *uint64, maxGoRoutines uint64) int {
 	if len(xs) == 3 {
 		if xs[2] > xs[0] && xs[2] > xs[1] { //peak is last
 			return xs[2]
@@ -23,6 +25,40 @@ func peakOfAList(xs []int) int {
 	} else {
 		//slice the array in half and send it off recursively
 		i := len(xs) / 2
-		return peakOfAList([]int{peakOfAList(xs[0:i]), peakOfAList(xs[i:])})
+		left := xs[0:i]
+		right := xs[i:]
+
+		async := func(xs []int, currentGoRoutines *uint64, maxGoRoutines uint64, f func(xss []int, currentGoRoutines *uint64, maxGoRoutines uint64) int, c chan int) {
+			go func() {
+				actual := f(xs, currentGoRoutines, maxGoRoutines)
+				c <- actual
+			}()
+		}
+		if atomic.LoadUint64(currentGoRoutines) < maxGoRoutines {
+			//fmt.Println("spawning a new go routine")
+			atomic.AddUint64(currentGoRoutines, 1)
+			//fmt.Printf("Current # go routines:%v\n", atomic.LoadUint64(currentGoRoutines))
+			c := make(chan int, 2)
+			async(left, currentGoRoutines, maxGoRoutines, peakOfAList, c)
+			async(right, currentGoRoutines, maxGoRoutines, peakOfAList, c)
+
+			diasResults := make([]int, 2)
+			diasResults[0], diasResults[1] = <-c, <-c
+
+			return peakOfAList([]int{diasResults[0], diasResults[1]}, currentGoRoutines, maxGoRoutines)
+		} else {
+			//fmt.Println("NOT spawning a new go routine")
+			a := peakOfAList(left, currentGoRoutines, maxGoRoutines)
+			b := peakOfAList(right, currentGoRoutines, maxGoRoutines)
+
+			return peakOfAList([]int{a, b}, currentGoRoutines, maxGoRoutines)
+		}
 	}
+}
+
+func PeakOfAList(xs []int, maxGoRoutines uint64) int {
+	var currentGoRoutines uint64
+	r := peakOfAList(xs, &currentGoRoutines, maxGoRoutines)
+	fmt.Printf("Go rountines: %v\n", currentGoRoutines)
+	return r
 }
