6683dfeb686105eb7677346120a0db31a67d6870
mikejlong60 <mikejlong60@gmail.com>
2022-12-02 12:38:37 -0500

    Still working on latest exercise

--- pkg/algorithms/chapter4/solved_exercise2.go
+++ pkg/algorithms/chapter4/solved_exercise2.go
@@ -34,32 +34,45 @@ var minS = func(xs []DayStockPrice) DayStockPrice {
 }
 
 var maxMinS = func(max, min DayStockPrice) []DayStockPrice {
-	if max.price > math.MinInt && min.price > math.MinInt && max.price > min.price { //&& max.day > min.day {
+	if max.price > math.MinInt && min.price > math.MinInt && max.price > min.price && max.day > min.day {
 		return []DayStockPrice{min, max}
+	} else if max.price > math.MinInt && min.price > math.MinInt && max.price > min.price && max.day < min.day {
+		return []DayStockPrice{max}
 	} else {
 		return []DayStockPrice{}
 	}
 }
 
-func MostProfit(xs []DayStockPrice) []DayStockPrice {
+func MostProfit(xs []DayStockPrice) []DayStockPrice { //If no solution return the min
 	if len(xs) == 0 {
 		return xs
-	} else if len(xs) == 1 {
-		return []DayStockPrice{}
+		//	} else if len(xs) == 1 {
+		//		return xs //[]DayStockPrice{}
 	} else if len(xs) == 2 {
+		//return xs
 		return maxMinS(maxS(xs), minS(xs))
 	} else if len(xs) == 3 { //return highest positive price difference over time or empty
 		return maxMinS(maxS(xs), minS(xs))
-	} else if len(xs) == 4 { //return highest positive price difference over time or empty
-		return maxMinS(maxS(xs), minS(xs))
+		//	} else if len(xs) == 4 { //return highest positive price difference over time or empty
+		//		return maxMinS(maxS(xs), minS(xs))
 	} else {
 		//slice the array in half and send it off recursively
 		i := len(xs) / 2
 		left := xs[0:i]
 		right := xs[i:]
 
-		a := MostProfit(left)
-		b := MostProfit(right)
+		var a, b []DayStockPrice
+		a = MostProfit(left)
+		b = MostProfit(right)
+		if len(a) == 0 {
+			a = []DayStockPrice{minS(left)}
+		}
+		if len(b) == 0 {
+			b = []DayStockPrice{maxS(right)}
+		}
+		//TODO
+		//If a is empty(no profit) make a the min of left.
+		//If b is empty(no profit) make b the max of right
 		return MostProfit(append(a, b...))
 	}
 }
--- pkg/algorithms/chapter4/solved_exercise2_test.go
+++ pkg/algorithms/chapter4/solved_exercise2_test.go
@@ -72,6 +72,50 @@ func TestMostProfitAscending6(t *testing.T) {
 	}
 }
 
+func TestMostProfitAscending8(t *testing.T) {
+	a := DayStockPrice{
+		day:   0,
+		price: 10,
+	}
+	b := DayStockPrice{
+		day:   1,
+		price: 11,
+	}
+
+	c := DayStockPrice{
+		day:   2,
+		price: 9,
+	}
+	d := DayStockPrice{
+		day:   3,
+		price: 5,
+	}
+	e := DayStockPrice{
+		day:   4,
+		price: 6,
+	}
+
+	f := DayStockPrice{
+		day:   5,
+		price: 14,
+	}
+
+	g := DayStockPrice{
+		day:   6,
+		price: 15,
+	}
+	h := DayStockPrice{
+		day:   7,
+		price: 17,
+	}
+	actual := MostProfit([]DayStockPrice{a, b, c, d, e, f, g, h})
+	expected := []DayStockPrice{e, h}
+
+	if !arrays.ArrayEquality(actual, expected, eq) {
+		t.Errorf("Actual:%v Expected:%v", actual, expected)
+	}
+}
+
 func TestMostProfitMiddle6(t *testing.T) {
 	a := DayStockPrice{
 		day:   0,
@@ -139,7 +183,7 @@ func TestMostProfitMiddle5(t *testing.T) {
 	}
 }
 
-func TestMostProfitDescending5(t *testing.T) {
+func TestNoProfitDescending5(t *testing.T) {
 	a := DayStockPrice{
 		day:   0,
 		price: 10,
