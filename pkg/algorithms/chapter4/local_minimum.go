caf0f3e1859736bba2ea8b5ffe6c2ce069cf2cb7
mikejlong60 <mikejlong60@gmail.com>
2023-03-01 09:36:10 -0500

    More tests

--- pkg/algorithms/chapter4/local_minimum.go
+++ pkg/algorithms/chapter4/local_minimum.go
@@ -9,22 +9,31 @@ type Node struct {
 
 var totalSteps int
 
-func LocalMinimum(currentMin int, guess *Node) int {
+func localMinimum(currentMin int, guess *Node) int {
 	totalSteps = totalSteps + 1
-	lesserValue := func(currentMin int, guess int) int {
+	lesserValue := func(currentMin, guess int) int {
 		if currentMin < guess {
 			return currentMin
 		} else {
 			return guess
 		}
 	}
+	var x int
+	if guess.Parent != nil {
+		x = lesserValue(currentMin, guess.Parent.Value)
+	} else {
+		x = currentMin
+	}
 
-	x := lesserValue(currentMin, guess.Parent.Value)
 	if guess.Left == nil || guess.Right == nil { //At a leaf of the complete binary tree
 		return lesserValue(x, guess.Value)
 	} else { //Divide and Conquer until you get to a leaf
-		l := LocalMinimum(x, guess.Left)
-		r := LocalMinimum(x, guess.Right)
+		l := localMinimum(x, guess.Left)
+		r := localMinimum(x, guess.Right)
 		return lesserValue(l, r)
 	}
+}
+
+func LocalMinimum(guess *Node) int {
+	return localMinimum(guess.Value, guess)
 }
--- pkg/algorithms/chapter4/local_minimum_test.go
+++ pkg/algorithms/chapter4/local_minimum_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestLocalMinimum(t *testing.T) {
+func TestLocalMinimumFromInnerNode(t *testing.T) {
 	a := Node{
 		Value: 14,
 	}
@@ -82,9 +82,179 @@ func TestLocalMinimum(t *testing.T) {
 	f.Right = &m
 	g.Left = &n
 	g.Right = &o
-	r := LocalMinimum(20, &b)
+	r := LocalMinimum(&b)
 	if r != 4 {
 		t.Errorf("Actual:%v Expected:%v", r, 4)
+	}
+
+	log.Infof("Local Minimum:%v, Total Steps:%v", r, totalSteps)
+}
+
+func TestLocalMinimumFromTopNode(t *testing.T) {
+	a := Node{
+		Value: 140,
+	}
+
+	b := Node{
+		Value: 130,
+	}
+	c := Node{
+		Value: 120,
+	}
+	d := Node{
+		Value: 110,
+	}
+	e := Node{
+		Value: 100,
+	}
+	f := Node{
+		Value: 90,
+	}
+	g := Node{
+		Value: 80,
+	}
+	h := Node{
+		Value: 70,
+	}
+	i := Node{
+		Value: 60,
+	}
+	j := Node{
+		Value: 50,
+	}
+	k := Node{
+		Value: 40,
+	}
+	l := Node{
+		Value: 3,
+	}
+	m := Node{
+		Value: 20,
+	}
+	n := Node{
+		Value: 10,
+	}
+	o := Node{
+		Value: 1800,
+	}
+	a.Left = &b
+	a.Right = &c
+	b.Parent = &a
+	c.Parent = &a
+	d.Parent = &b
+	e.Parent = &b
+	f.Parent = &c
+	g.Parent = &c
+
+	h.Parent = &d
+	i.Parent = &d
+	j.Parent = &e
+	k.Parent = &e
+	l.Parent = &f
+	m.Parent = &f
+	n.Parent = &g
+	o.Parent = &g
+
+	b.Left = &d
+	b.Right = &e
+	c.Left = &f
+	c.Right = &g
+	d.Left = &h
+	d.Right = &i
+	e.Left = &j
+	e.Right = &k
+	f.Left = &l
+	f.Right = &m
+	g.Left = &n
+	g.Right = &o
+	r := LocalMinimum(&a)
+	if r != 3 {
+		t.Errorf("Actual:%v Expected:%v", r, 3)
+	}
+
+	log.Infof("Local Minimum:%v, Total Steps:%v", r, totalSteps)
+}
+
+func TestLocalMinimumFromLeafNode(t *testing.T) {
+	a := Node{
+		Value: 140,
+	}
+
+	b := Node{
+		Value: 130,
+	}
+	c := Node{
+		Value: 120,
+	}
+	d := Node{
+		Value: 110,
+	}
+	e := Node{
+		Value: 100,
+	}
+	f := Node{
+		Value: 90,
+	}
+	g := Node{
+		Value: 80,
+	}
+	h := Node{
+		Value: 70,
+	}
+	i := Node{
+		Value: 60,
+	}
+	j := Node{
+		Value: 50,
+	}
+	k := Node{
+		Value: 40,
+	}
+	l := Node{
+		Value: 3,
+	}
+	m := Node{
+		Value: 20,
+	}
+	n := Node{
+		Value: 10,
+	}
+	o := Node{
+		Value: 1800,
+	}
+	a.Left = &b
+	a.Right = &c
+	b.Parent = &a
+	c.Parent = &a
+	d.Parent = &b
+	e.Parent = &b
+	f.Parent = &c
+	g.Parent = &c
+
+	h.Parent = &d
+	i.Parent = &d
+	j.Parent = &e
+	k.Parent = &e
+	l.Parent = &f
+	m.Parent = &f
+	n.Parent = &g
+	o.Parent = &g
+
+	b.Left = &d
+	b.Right = &e
+	c.Left = &f
+	c.Right = &g
+	d.Left = &h
+	d.Right = &i
+	e.Left = &j
+	e.Right = &k
+	f.Left = &l
+	f.Right = &m
+	g.Left = &n
+	g.Right = &o
+	r := LocalMinimum(&o)
+	if r != 80 {
+		t.Errorf("Actual:%v Expected:%v", r, 80)
 	}
 
 	log.Infof("Local Minimum:%v, Total Steps:%v", r, totalSteps)
