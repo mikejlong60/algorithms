af0a0695a52f8f31278b5ca27bba9e1f0617c070
mikejlong60 <mikejlong60@gmail.com>
2023-10-02 09:28:54 -0400

    Completes Prim's algorithm

--- pkg/algorithms/chapter4/prims.go
+++ pkg/algorithms/chapter4/prims.go
@@ -7,21 +7,21 @@ import (
 )
 
 type PrimsEdge struct {
-	u      *PrimsNode
-	v      *PrimsNode
+	u      string
+	v      string
 	length int
 }
 
 type PrimsNode struct {
 	id            string
-	connectionsTo heap.Heap[PrimsEdge, *PrimsNode]
+	connectionsTo heap.Heap[PrimsEdge, string]
 }
 
 func (w PrimsNode) String() string {
 	return fmt.Sprintf("PrimsNode{id:%v, connectionsTo:%v}", w.id, w.connectionsTo)
 }
 func (w PrimsEdge) String() string {
-	return fmt.Sprintf("PrimsEdge{u:%v, v:%v, length:%v}", w.u.id, w.v.id, w.length)
+	return fmt.Sprintf("PrimsEdge{u:%v, v:%v, length:%v}", w.u, w.v, w.length)
 }
 
 func primsEdgeLt(l, r *PrimsEdge) bool {
@@ -32,60 +32,64 @@ func primsEdgeLt(l, r *PrimsEdge) bool {
 	}
 }
 
-func primsNodeLt(l, r *PrimsNode) bool {
-	if l.id < r.id {
-		return true
-	} else {
-		return false
-	}
-}
-
-func extractor(edge *PrimsEdge) *PrimsNode {
+func extractor(edge *PrimsEdge) string {
 	return edge.v
 }
 
-func minSpanningTree(xs []*PrimsNode, xxs []*PrimsNode) ([]*PrimsNode, []*PrimsNode) {
-	//1. Is xss len == xs len ? If true return
-	//2. Find minimum-cost connected node among all edges in map in xss.
-	//2. Add that node to xxs
-	//3 Remove all nodes from connections pointing back to any nodes in xs
-	//4. call minSpanningTree again with xs and updated xxs
+func minSpanningTree(xs []*PrimsNode, xxs []*PrimsEdge) ([]*PrimsNode, []*PrimsEdge) {
+	//1. Is minEdge == nil return
+	//2. Find minimum-cost connected edge(minEdge) among all edges in array of PrimsNodes.
+	//3. Delete all edges that point to minEdge.v in original xs array.
+	//4. Add that minEdge to PrimsEdge result (xxs) array
+	//4. call minSpanningTree again with updated xs and xxs
 
-	minNode := func(xs []*PrimsNode) *PrimsNode {
-		var m = &PrimsEdge{length: math.MaxInt}
+	deleteVEdge := func(xs []*PrimsNode, edge *PrimsEdge) []*PrimsNode {
 		for _, y := range xs {
-			b, err := heap.FindMin(y.connectionsTo)
-			if err == nil && b.length < m.length {
-				m = b
-			}
-		}
-		return m.v
-	}
-
-	removeV := func(xs []*PrimsNode, m *PrimsNode) []*PrimsNode {
-		for _, y := range xs {
-			b := heap.FindPosition(y.connectionsTo, m)
-			if b != -1 {
-				conns, _ := heap.HeapDelete(y.connectionsTo, b, primsEdgeLt)
-				y.connectionsTo = conns
+			p := heap.FindPosition(y.connectionsTo, edge.v)
+			if p > -1 {
+				y.connectionsTo, _ = heap.HeapDelete(y.connectionsTo, p, primsEdgeLt)
 			}
 		}
 		return xs
 	}
 
-	if len(xs) == len(xxs) {
-		return xs, xxs
-	} else {
-		m := minNode(xxs)
-		xxs = append(xxs, m)
-		newXs := removeV(xs, m)
-		return minSpanningTree(newXs, xxs)
+	minEdge := func(xxs []*PrimsNode) *PrimsEdge {
+		var lowestEdge = &PrimsEdge{
+			u:      "",
+			v:      "",
+			length: math.MaxInt,
+		}
+		for _, y := range xxs {
+			a, err := heap.FindMin(y.connectionsTo)
+			if err == nil && a.length < lowestEdge.length {
+				lowestEdge = a
+			}
+		}
+
+		if lowestEdge.length == math.MaxInt {
+			return nil
+		} else {
+			return lowestEdge
+		}
 	}
+
+	e := minEdge(xs)
+	if e == nil {
+		return xs, xxs
+	}
+	xs = deleteVEdge(xs, e)
+	xxs = append(xxs, e)
+	return minSpanningTree(xs, xxs)
 }
 
-func MinSpanningTree(xs []*PrimsNode) []*PrimsNode {
-	var xxs = []*PrimsNode{}
-	xxs = append(xxs, xs[0])
-	_, r := minSpanningTree(xs, xxs)
-	return r
+func MinSpanningTree(xs []*PrimsNode) ([]*PrimsEdge, int) {
+	totalCost := func(xs []*PrimsEdge) int {
+		var r int
+		for _, b := range xs {
+			r = b.length + r
+		}
+		return r
+	}
+	_, r := minSpanningTree(xs, []*PrimsEdge{})
+	return r, totalCost(r)
 }
--- pkg/algorithms/chapter4/prims_test.go
+++ pkg/algorithms/chapter4/prims_test.go
@@ -1,94 +1,95 @@
 package chapter4
 
 import (
-	"fmt"
 	"github.com/greymatter-io/golangz/heap"
 	"testing"
 )
 
 func TestMinSpanningTree(t *testing.T) {
 
-	a := &PrimsNode{id: "a", connectionsTo: heap.New[PrimsEdge, *PrimsNode](extractor)}
-	b := &PrimsNode{id: "b", connectionsTo: heap.New[PrimsEdge, *PrimsNode](extractor)}
-	c := &PrimsNode{id: "c", connectionsTo: heap.New[PrimsEdge, *PrimsNode](extractor)}
-	d := &PrimsNode{id: "d", connectionsTo: heap.New[PrimsEdge, *PrimsNode](extractor)}
-	e := &PrimsNode{id: "e", connectionsTo: heap.New[PrimsEdge, *PrimsNode](extractor)}
-	f := &PrimsNode{id: "f", connectionsTo: heap.New[PrimsEdge, *PrimsNode](extractor)}
-	g := &PrimsNode{id: "g", connectionsTo: heap.New[PrimsEdge, *PrimsNode](extractor)}
+	a := &PrimsNode{id: "a", connectionsTo: heap.New[PrimsEdge, string](extractor)}
+	b := &PrimsNode{id: "b", connectionsTo: heap.New[PrimsEdge, string](extractor)}
+	c := &PrimsNode{id: "c", connectionsTo: heap.New[PrimsEdge, string](extractor)}
+	d := &PrimsNode{id: "d", connectionsTo: heap.New[PrimsEdge, string](extractor)}
+	e := &PrimsNode{id: "e", connectionsTo: heap.New[PrimsEdge, string](extractor)}
+	f := &PrimsNode{id: "f", connectionsTo: heap.New[PrimsEdge, string](extractor)}
+	g := &PrimsNode{id: "g", connectionsTo: heap.New[PrimsEdge, string](extractor)}
 	ab := &PrimsEdge{
-		u:      a,
-		v:      b,
+		u:      a.id,
+		v:      b.id,
 		length: 2,
 	}
 	ac := &PrimsEdge{
-		u:      a,
-		v:      c,
+		u:      a.id,
+		v:      c.id,
 		length: 3,
 	}
 	ad := &PrimsEdge{
-		u:      a,
-		v:      d,
+		u:      a.id,
+		v:      d.id,
 		length: 3,
 	}
 
 	bc := &PrimsEdge{
-		u:      b,
-		v:      c,
+		u:      b.id,
+		v:      c.id,
 		length: 4,
 	}
 	be := &PrimsEdge{
-		u:      b,
-		v:      e,
+		u:      b.id,
+		v:      e.id,
 		length: 3,
 	}
 
 	cd := &PrimsEdge{
-		u:      c,
-		v:      d,
+		u:      c.id,
+		v:      d.id,
 		length: 5,
 	}
 	cf := &PrimsEdge{
-		u:      c,
-		v:      f,
+		u:      c.id,
+		v:      f.id,
 		length: 6,
 	}
 	ce := &PrimsEdge{
-		u:      c,
-		v:      e,
+		u:      c.id,
+		v:      e.id,
 		length: 1,
 	}
 
 	df := &PrimsEdge{
-		u:      d,
-		v:      f,
+		u:      d.id,
+		v:      f.id,
 		length: 7,
 	}
 
 	ef := &PrimsEdge{
-		u:      e,
-		v:      f,
+		u:      e.id,
+		v:      f.id,
 		length: 8,
 	}
 
 	fg := &PrimsEdge{
-		u:      f,
-		v:      g,
+		u:      f.id,
+		v:      g.id,
 		length: 9,
 	}
 
-	a.connectionsTo = heap.HeapInsert[PrimsEdge, *PrimsNode](a.connectionsTo, ab, primsEdgeLt)
-	a.connectionsTo = heap.HeapInsert[PrimsEdge, *PrimsNode](a.connectionsTo, ac, primsEdgeLt)
-	a.connectionsTo = heap.HeapInsert[PrimsEdge, *PrimsNode](a.connectionsTo, ad, primsEdgeLt)
-	b.connectionsTo = heap.HeapInsert[PrimsEdge, *PrimsNode](b.connectionsTo, bc, primsEdgeLt)
-	b.connectionsTo = heap.HeapInsert[PrimsEdge, *PrimsNode](b.connectionsTo, be, primsEdgeLt)
-	c.connectionsTo = heap.HeapInsert[PrimsEdge, *PrimsNode](c.connectionsTo, cd, primsEdgeLt)
-	c.connectionsTo = heap.HeapInsert[PrimsEdge, *PrimsNode](c.connectionsTo, ce, primsEdgeLt)
-	c.connectionsTo = heap.HeapInsert[PrimsEdge, *PrimsNode](c.connectionsTo, cf, primsEdgeLt)
-	d.connectionsTo = heap.HeapInsert[PrimsEdge, *PrimsNode](d.connectionsTo, df, primsEdgeLt)
-	e.connectionsTo = heap.HeapInsert[PrimsEdge, *PrimsNode](e.connectionsTo, ef, primsEdgeLt)
-	f.connectionsTo = heap.HeapInsert[PrimsEdge, *PrimsNode](f.connectionsTo, fg, primsEdgeLt)
-	fmt.Println(heap.FindMin(f.connectionsTo))
-	actual := MinSpanningTree([]*PrimsNode{a, b, c, d, e, f, g})
-	fmt.Println(actual)
+	a.connectionsTo = heap.HeapInsert[PrimsEdge, string](a.connectionsTo, ab, primsEdgeLt)
+	a.connectionsTo = heap.HeapInsert[PrimsEdge, string](a.connectionsTo, ac, primsEdgeLt)
+	a.connectionsTo = heap.HeapInsert[PrimsEdge, string](a.connectionsTo, ad, primsEdgeLt)
+	b.connectionsTo = heap.HeapInsert[PrimsEdge, string](b.connectionsTo, bc, primsEdgeLt)
+	b.connectionsTo = heap.HeapInsert[PrimsEdge, string](b.connectionsTo, be, primsEdgeLt)
+	c.connectionsTo = heap.HeapInsert[PrimsEdge, string](c.connectionsTo, cd, primsEdgeLt)
+	c.connectionsTo = heap.HeapInsert[PrimsEdge, string](c.connectionsTo, ce, primsEdgeLt)
+	c.connectionsTo = heap.HeapInsert[PrimsEdge, string](c.connectionsTo, cf, primsEdgeLt)
+	d.connectionsTo = heap.HeapInsert[PrimsEdge, string](d.connectionsTo, df, primsEdgeLt)
+	e.connectionsTo = heap.HeapInsert[PrimsEdge, string](e.connectionsTo, ef, primsEdgeLt)
+	f.connectionsTo = heap.HeapInsert[PrimsEdge, string](f.connectionsTo, fg, primsEdgeLt)
+	_, totalCost := MinSpanningTree([]*PrimsNode{a, b, c, d, e, f, g}) //Total cost should be 24
+
+	if totalCost != 24 {
+		t.Errorf("Actual total cost:%v, expected total cost:%v", totalCost, 24)
+	}
 
 }
