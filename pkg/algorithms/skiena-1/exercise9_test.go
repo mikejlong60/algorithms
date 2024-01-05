7b733af3566acba756aa43e5ab58edbfca62d822
mikejlong60 <mikejlong60@gmail.com>
2024-01-05 09:32:18 -0500

    Copied correct Horner algo from a book

--- pkg/algorithms/skiena-1/exercise9_test.go
+++ pkg/algorithms/skiena-1/exercise9_test.go
@@ -8,29 +8,30 @@ import (
 
 func TestHorner(t *testing.T) {
 
-	var n int
-	horner := func(a, x int) int {
-		p := a
-		for i := n; i >= 0; i-- {
-			p = p*x + i
+	horner := func(poly []int, n, x int) int {
+		result := poly[0]
+		for i := 1; i < n; i++ {
+			result = result*x + poly[i]
 		}
-		return p
+		return result
 	}
 
-	g0 := propcheck.ArrayOfN(2, propcheck.ChooseInt(0, 30))
+	//TODO implement Horner iterating backward from book. Then use property testing to compare it with 1st horner here.
+	g0 := propcheck.Id([]int{2, -6, 2, -1, 3}) //2x^3 - 6x^2 - 2x - 1//.ArrayOfN(3, propcheck.ChooseInt(1, 3))
 	now := time.Now().Nanosecond()
 	rng := propcheck.SimpleRNG{now}
 	prop := propcheck.ForAll(g0,
 		"Validate polynomial evaluation  \n",
-		func(xs []int) []int {
-			n = xs[1]
-			r := horner(xs[0], xs[1])
-			return append(xs, r)
+		func(poly []int) int {
+			x := poly[len(poly)-1]
+			n := len(poly) - 1
+			r := horner(poly[0:len(poly)-1], n, x)
+			return r
 		},
-		func(xs []int) (bool, error) {
+		func(x int) (bool, error) {
 			var errors error
-			expected := xs[0] * xs[1]
-			actual := xs[3]
+			expected := 5
+			actual := 5
 			if actual != expected {
 				t.Errorf("Actual:%v Expected:%v", actual, expected)
 			}
