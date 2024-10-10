fc3c88db7215ccd5f4376a955b0a6560f7e6ff3b
mikejlong60 <mikejlong60@gmail.com>
2024-10-10 09:33:04 -0400

    adfsdf

--- skiena-4/hashmap.h
+++ skiena-4/hashmap.h
@@ -44,20 +44,25 @@ struct PublisherNlist *lookup(char *s) {
 }
 
 // install - put(name defn) in hashtab
-struct PublisherNlist *install(char *name, char *defn) {
+struct PublisherNlist *install(char *name, char *defn)
+{
     struct PublisherNlist *np;
     unsigned hashval;
-    if ((np = lookup(name)) == NULL) { //not found
+    if ((np = lookup(name)) == NULL)
+    { //not found
         np = (struct PublisherNlist *) malloc(sizeof(*np));
         if (np == NULL || (np->name = strdup(name)) == NULL)
             return NULL;
         hashval = hash(name);
         np->next = hashtab[hashval];
         hashtab[hashval] = np;
-    } else { //already there
+    }
+    else
+    { //already there
         free((void *) np->defn); //free previous defn
     }
-    if ((np->defn = strdup(defn)) == NULL) {
+    if ((np->defn = strdup(defn)) == NULL)
+    {
         return NULL;
     }
     return np;
--- skiena-4/inthashmap.h
+++ skiena-4/inthashmap.h
@@ -38,67 +38,28 @@ unsigned hash(const int s)
 //lookup
 struct Nlist* lookup(const int s)
 {
-    printf("lookup1\n");
     for (struct Nlist* np = hashtab[hash(s)]; np != NULL; np = np->next)
     {
-        printf("searching linked list in cell[%d]\n", hash(s));
         if (s == np->key)
         {
-            printf("found in linked list\n");
             return np; //found
         }
     }
-    printf("did not find in lookup in cell[%d]\n", hash(s));
     return NULL; //not found
 }
 
 struct Nlist* install(const int key, const int value)
 {
-    printf("install1:key[%d] --- value[%d]\n", key, value);
     struct Nlist* np;
-    printf("install111\n");
     if ((np = lookup(key)) == NULL)
     {
         //not found
-        printf("install2\n");
         np = (struct Nlist*)malloc(sizeof(*np));
-        if (np == NULL || (np->key == key))
-            return NULL;
+        np->key = key;
         unsigned hashval = hash(key);
         np->next = hashtab[hashval];
-        printf("hashval[%d]\n", hashval);
         hashtab[hashval] = np;
     }
-    else
-    {
-        //already there
-        printf("install3\n");
-        free((void*)np->value); //free previous defn
-        printf("install4\n");
-    }
-    printf("install5\n");
-    if (np->value == value)
-    {
-        printf("install6\n");
-        return NULL;
-    }
+    np->value = value;
     return np;
 }
-
-
-// inline int main() {
-//     printf("1\n");
-//     for (int i = 0; i < NUMELMTS; i++) {
-//         //char key[20];
-//         //sprintf(key, "MAX%d", i);
-//         //char val[30];
-//         //sprintf(val, "fred%d", i);
-//     //printf("2\n");
-//         struct Nlist *x = install(&i, &i);
-//         printf("3\n");
-//     }
-//     for (int i = 0; i < NUMELMTS; i++) {
-//         printf("in loop\n");
-//         Nlistprint(hashtab[i]);
-//     }
-// }
