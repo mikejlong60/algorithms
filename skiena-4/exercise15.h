43d9592d66df955cdf3aa90a2b78df5733f3a874
mikejlong60 <mikejlong60@gmail.com>
2024-11-26 09:53:53 -0500

    adsfas

--- skiena-4/exercise15.h
+++ skiena-4/exercise15.h
@@ -7,35 +7,8 @@ Give a O(n log n) algorithm to identify a point p on the line which is in the la
 As an example, for S = {(10,40), (20,60), (50,90), (15,70)}, no point exists in all 4 intervals.
 But 50 exists in 3 intervals. You can assume an endpoint counts as being in its own interval(inclusive).
 
-Algorithm Efficiency - 
+Algorithm Efficiency -  O(n^2) -- it sucks I know.
 
-S sorted by l: S = {(10,40), (15, 70), (20,60), (50, 90)}
-S sorted by r: S = {(10,40), (20,60), (15, 70), (50, 90)}
-S l sorted: Sl = {10, 15, 20, 50}
-S r sorted: Sl = {40, 60, 70, 90}
-
-
-Algorithm:
-struct currentMax {
-   l int
-   r int
-   numOverlaps int
-}
-1. sort S by l
-2. Set resultMax = currentMax{}
-3. resultMax.l = S[0].l;
-4. resultMax.r = s[0].r;
-5. resultMax.numOverlaps = 1;
-6. for int i := 1; i < len(s); i++ {
-   maybeMax = currentMax{};
-   if S[i].l <= currentMax.r && currentMax{
-      currentMax.numOverlaps++;
-   } else {
-      currentMax.l = S[i].l
-      currentMax.r = S[i].r
-      currentMax.numOverlaps = 1
-   }
-}
 */
 
 struct Max {
@@ -49,40 +22,31 @@ struct Interval {
     int r;
 };
 
-struct Max mostFrequentPoint(const struct Interval* S, const int n) {
-    struct Max currentOverlap; //the interval that has an overlap with subsequent intervals
-    currentOverlap.l = 0;
-    currentOverlap.r = 0;
-    currentOverlap.numOverlaps = 0;
-    struct Max maxOverlap;//the current maximum overlap
-    maxOverlap.l = 0;
-    maxOverlap.r = 0;
-    maxOverlap.numOverlaps = 0;
-    for (int i = 0; i < n; i++) {
-        if (i == 0) {//First element
-            currentOverlap.l = S[i].l;
-            currentOverlap.r = S[i].r;
-            currentOverlap.numOverlaps = 1;
-            maxOverlap.l = S[i].l;
-            maxOverlap.r = S[i].r;
-            maxOverlap.numOverlaps = 1;
-        } else
+char mostFrequentPoint(const struct Interval* S, const int n)  {
+    for (int a = 0; a < n; a++) {
+        for (int b = 0; b < n; b++)
         {
-            //Not at first element
-            if (S[i].l <= currentOverlap.r) {//Is there an overlap with next element from currentOverlap
-                currentOverlap.numOverlaps += 1;
-            } else {// begin new overlap calculation
-                if (currentOverlap.numOverlaps > maxOverlap.numOverlaps) {//new max overlap
-                    maxOverlap = currentOverlap;
-                }
-                currentOverlap.l = S[i].l;
-                currentOverlap.r = S[i].r;
-                currentOverlap.numOverlaps = 1;
+            if (S[b].l >= S[a].l && S[b].r > S[a].r) {
+                //increment b.l point total
+                printf("\n1 - S[b].l point total:%d ",S[b].l);
+            } else if(S[b].l >= S[a].l && S[b].r <= S[a].r) {
+                //increment b.l point total
+                printf("\n2 - S[b].l point total:%d ",S[b].l);
+            } else if (S[b].l < S[a].l && S[b].r <= S[a].r) {
+                //increment b.r point total
+                printf("\n3 - S[b].r point total:%d ",S[b].r);
+            } else if (S[b].l > S[a].r) {
+                //can't increment anything, no overlap
+                printf("\n4 - no overlap");
+
+            } else if (S[b].l < S[a].l && S[b].r > S[a].r) {
+                //increment a.l point total
+                printf("\n5 - S[a].l point total:%d ",S[b].l);
             }
         }
     }
 
-    return  maxOverlap;
+    return  "fred";
 }
   // Comparison function for intervals
 int compareIntervals(const void *a, const void *b) {
@@ -111,9 +75,9 @@ int testMostFrequentPoint() {
     int n = sizeof(S) / sizeof(S[0]);
 
     // Use qsort to sort the array
-    qsort(S, n, sizeof(struct Interval), compareIntervals);
+    //qsort(S, n, sizeof(struct Interval), compareIntervals);
 
-    struct Max result = mostFrequentPoint(S, n);
-    assert(result.l == 50);
-    assert(result.r == 90);
+    char result = mostFrequentPoint(S, n);
+    assert(result == 50);
+    //assert(result.r == 90);
 }
